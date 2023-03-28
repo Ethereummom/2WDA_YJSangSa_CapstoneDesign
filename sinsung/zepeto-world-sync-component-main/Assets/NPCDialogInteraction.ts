59749fa76d775257e2eecd3556566d8ac0d43e92
ss <rkdtlstjd123@naver.com>
2023-03-28 16:47:52 +0900

    ss
    ss

---
+++ sinsung/zepeto-world-sync-component-main/Assets/NPCDialogInteraction.ts
@@ -0,0 +1,55 @@
+import { GameObject, HumanBodyBones, Object, Collider } from 'UnityEngine';
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script';
+import { Button } from 'UnityEngine.UI';
+import { ZepetoCharacter, ZepetoPlayers } from "ZEPETO.Character.Controller";
+
+import {  } from "./World.multiplay/index";
+
+export default class NPCDialogInteraction extends ZepetoScriptBehaviour {
+
+    public npcDialogCanvas: GameObject;
+    public yesButton: Button;
+    public noButton: Button;
+
+    private _zepetoCharacter: ZepetoCharacter;
+
+    Start() {
+
+        ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(()=>{
+            this._zepetoCharacter = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer.character;
+        });
+        
+        // Dialog - Select Yes
+        this.yesButton.onClick.AddListener(() => {
+            console.log("yes")
+            this.npcDialogCanvas.SetActive(false);
+            const zepetoUserId : String = "yes";
+            // RequestUserId(zepetoUserId)
+
+            
+        });
+
+        // Dialog - Select No
+        this.noButton.onClick.AddListener(() => {
+            console.log("no")
+            this.npcDialogCanvas.SetActive(false);
+        });
+
+    }
+
+    // Check if Player character enter collider
+    OnTriggerEnter(collider: Collider) {
+        if (this._zepetoCharacter == null || collider.gameObject != this._zepetoCharacter.gameObject) {
+            return;
+        }
+        this.npcDialogCanvas.SetActive(true);
+    }
+
+    OnTriggerExit(collider: Collider) {
+        if (this._zepetoCharacter == null || collider.gameObject != this._zepetoCharacter.gameObject) {
+            return;
+        }
+        this.npcDialogCanvas.SetActive(false);
+    }
+
+}
---
+++ sinsung/zepeto-world-sync-component-main/Assets/World.multiplay/index.ts
@@ -0,0 +1,381 @@
+import {Sandbox, SandboxOptions, SandboxPlayer} from "ZEPETO.Multiplay";
+import {Player, sVector3, sQuaternion, SyncTransform, PlayerAdditionalValue, ZepetoAnimationParam} from "ZEPETO.Multiplay.Schema";
+import { HttpContentType, HttpService, HttpResponse, HttpHeader, HttpBodyType } from "ZEPETO.Multiplay.HttpService";
+import { WorldService } from "ZEPETO.World";
+import { ZepetoWorldMultiplay } from 'ZEPETO.World';
+export { onJoin };
+
+
+
+export default class extends Sandbox {
+    private sessionIdQueue: string[] = [];
+    private InstantiateObjCaches : InstantiateObj[] = [];
+    private masterClient = () => this.loadPlayer(this.sessionIdQueue[0]);
+
+
+    onCreate(options: SandboxOptions) {
+
+        // const data = {
+        //     name : 'kimyuhyeon',
+        //     challenge:'wake up'
+        // };
+
+        // const url = "http://ec2-13-124-144-98.ap-northeast-2.compute.amazonaws.com/index.php";
+
+        // const option = {
+        //     method : "POST",
+        //     headers:{
+        //         "Content-Type":"application/json",
+        //     },
+        //     body:JSON.stringify(data),
+        // };
+
+        // fetch(url,option)
+        // .then((response)=>response.json())
+        // .then((data) => console.log(data))
+        // .then((error)=>console.log(error));
+
+
+        // -----------------------------------------------------------------------------------------------------------------
+        
+
+        // -----------------------------------------------------------------------------------------------------------------
+
+
+
+        // this.onMessage('save', async (client, message) => {
+        //     // const request = await HttpService.getAsync('https://.../%27', )
+        //     const request = await HttpService.getAsync('http://13.124.226.182:8080/member01/')
+        //     if (request.statusCode !== 200) {
+        //         console.error('HTTP ERROR');
+        //         return;
+        //     }
+
+        //     const json = JSON.parse(request.response);
+        //     // json 내용 처리
+        // });
+
+
+
+
+        /**Zepeto Player Sync**/
+        this.onMessage(MESSAGE.SyncPlayer, (client, message) => {
+            const player = this.state.players.get(client.sessionId);
+            if(player) {
+                /** State **/
+                //animation param
+                const animationParam = new ZepetoAnimationParam();
+                animationParam.State = message.animationParam.State;
+                animationParam.MoveState = message.animationParam.MoveState;
+                animationParam.JumpState = message.animationParam.JumpState;
+                animationParam.LandingState = message.animationParam.LandingState;
+                animationParam.MotionSpeed = message.animationParam.MotionSpeed;
+                animationParam.FallSpeed = message.animationParam.FallSpeed;
+                animationParam.Acceleration = message.animationParam.Acceleration;
+                animationParam.MoveProgress = message.animationParam.MoveProgress;
+                player.animationParam = animationParam;
+
+                player.gestureName = message.gestureName; // Gesture Sync
+
+                //additional Value
+                if (message.playerAdditionalValue != null) {
+                    const pAdditionalValue = new PlayerAdditionalValue();
+                    pAdditionalValue.additionalWalkSpeed = message.playerAdditionalValue.additionalWalkSpeed;
+                    pAdditionalValue.additionalRunSpeed = message.playerAdditionalValue.additionalRunSpeed;
+                    pAdditionalValue.additionalJumpPower = message.playerAdditionalValue.additionalJumpPower;
+                    player.playerAdditionalValue = pAdditionalValue;
+                }
+            }
+        });
+
+        /**Transform Sync**/
+        this.onMessage(MESSAGE.SyncTransform, (client, message) => {
+            if (!this.state.SyncTransforms.has(message.Id)) {
+                const syncTransform = new SyncTransform();
+                this.state.SyncTransforms.set(message.Id.toString(), syncTransform);
+            }
+            const syncTransform:SyncTransform = this.state.SyncTransforms.get(message.Id);
+            if(syncTransform) {
+                syncTransform.Id = message.Id;
+                syncTransform.position = new sVector3();
+                syncTransform.position.x = message.position.x;
+                syncTransform.position.y = message.position.y;
+                syncTransform.position.z = message.position.z;
+
+                syncTransform.localPosition = new sVector3();
+                syncTransform.localPosition.x = message.localPosition.x;
+                syncTransform.localPosition.y = message.localPosition.y;
+                syncTransform.localPosition.z = message.localPosition.z;
+
+                syncTransform.rotation = new sQuaternion();
+                syncTransform.rotation.x = message.rotation.x;
+                syncTransform.rotation.y = message.rotation.y;
+                syncTransform.rotation.z = message.rotation.z;
+                syncTransform.rotation.w = message.rotation.w;
+
+                syncTransform.scale = new sVector3();
+                syncTransform.scale.x = message.scale.x;
+                syncTransform.scale.y = message.scale.y;
+                syncTransform.scale.z = message.scale.z;
+
+                syncTransform.sendTime = message.sendTime;
+            }
+        });
+        this.onMessage(MESSAGE.SyncTransformStatus, (client, message) => {
+            const syncTransform:SyncTransform = this.state.SyncTransforms.get(message.Id);
+            if(syncTransform)
+                syncTransform.status = message.Status;
+        });
+
+        this.onMessage(MESSAGE.ChangeOwner, (client,message:string) => {
+            this.broadcast(MESSAGE.ChangeOwner+message, client.sessionId);
+        });
+        this.onMessage(MESSAGE.Instantiate, (client,message:InstantiateObj) => {
+            const InstantiateObj: InstantiateObj = {
+                Id: message.Id,
+                prefabName: message.prefabName,
+                ownerSessionId: message.ownerSessionId,
+                spawnPosition: message.spawnPosition,
+                spawnRotation: message.spawnRotation,
+            };
+            this.InstantiateObjCaches.push(InstantiateObj);
+            this.broadcast(MESSAGE.Instantiate, InstantiateObj);
+        });
+        this.onMessage(MESSAGE.RequestInstantiateCache, (client) => {
+            this.InstantiateObjCaches.forEach((obj)=>{
+                client.send(MESSAGE.Instantiate, obj);
+            });
+        });
+
+        /**SyncDOTween**/
+        this.onMessage(MESSAGE.SyncDOTween, (client, message: syncTween) => {
+            const tween: syncTween = {
+                Id: message.Id,
+                position: message.position,
+                nextIndex: message.nextIndex,
+                loopCount: message.loopCount,
+                sendTime: message.sendTime,
+            };
+            this.broadcast(MESSAGE.ResponsePosition + message.Id, tween, {except: this.masterClient()});
+        });
+
+        /**Common**/
+        this.onMessage(MESSAGE.CheckServerTimeRequest, (client, message) => {
+            let Timestamp = +new Date();
+            client.send(MESSAGE.CheckServerTimeResponse, Timestamp);
+        });
+        this.onMessage(MESSAGE.CheckMaster, (client, message) => {
+            console.log(`master->, ${this.sessionIdQueue[0]}`);
+            this.broadcast(MESSAGE.MasterResponse, this.sessionIdQueue[0]);
+        });
+        this.onMessage(MESSAGE.PauseUser, (client) => {
+            if(this.sessionIdQueue.includes(client.sessionId)) {
+                const pausePlayerIndex = this.sessionIdQueue.indexOf(client.sessionId);
+                this.sessionIdQueue.splice(pausePlayerIndex, 1);
+                
+                if (pausePlayerIndex == 0) {
+                    console.log(`master->, ${this.sessionIdQueue[0]}`);
+                    this.broadcast(MESSAGE.MasterResponse, this.sessionIdQueue[0]);
+                }
+            }
+        });
+        this.onMessage(MESSAGE.UnPauseUser, (client) => {
+            if(!this.sessionIdQueue.includes(client.sessionId)) {
+                this.sessionIdQueue.push(client.sessionId);
+                this.broadcast(MESSAGE.MasterResponse, this.sessionIdQueue[0]);
+            }
+        });
+        
+        /** Sample Code **/
+        this.onMessage(MESSAGE.BlockEnter, (client,transformId:string) => {
+            this.broadcast(MESSAGE.BlockEnter+transformId, client.sessionId);
+        });
+        this.onMessage(MESSAGE.BlockExit, (client,transformId:string) => {
+            this.broadcast(MESSAGE.BlockExit+transformId, client.sessionId);
+        });
+        this.onMessage(MESSAGE.SendBlockEnterCache, (client,blockCache) => {
+            this.loadPlayer(blockCache.newJoinSessionId)?.send(MESSAGE.BlockEnter+blockCache.transformId, client.sessionId);
+        });
+        
+        this.onMessage(MESSAGE.CoinAcquired, (client,transformId:string) => {
+            this.masterClient()?.send(MESSAGE.CoinAcquired+transformId, client.sessionId);
+        });
+        
+        /** Racing Game **/
+        let isStartGame:boolean = false;
+        let startServerTime:number;
+        this.onMessage(MESSAGE.StartRunningRequest, (client) => {
+            if(!isStartGame) {
+                isStartGame = true;
+                startServerTime = +new Date();
+
+                this.broadcast(MESSAGE.CountDownStart, startServerTime);
+            }
+        });
+        this.onMessage(MESSAGE.FinishPlayer, (client,finishTime:number) => {
+            let playerLapTime = (finishTime-startServerTime)/1000;
+            console.log(`${client.sessionId}is enter! ${playerLapTime}`);
+            const gameReport: GameReport = {
+                playerUserId: client.userId,
+                playerLapTime: playerLapTime,
+            };
+            this.broadcast(MESSAGE.ResponseGameReport, gameReport);
+            if(isStartGame) {
+                isStartGame = false;
+                let gameEndTime:number = +new Date();
+                this.broadcast(MESSAGE.FirstPlayerGetIn, gameEndTime);
+            }
+        });
+    }
+
+    onJoin(client: SandboxPlayer) {
+        const player = new Player();
+        player.sessionId = client.sessionId;
+        if (client.hashCode) {
+            player.zepetoHash = client.hashCode;
+        }
+        if (client.userId) {
+            player.zepetoUserId = client.userId;
+        }
+        const players = this.state.players;
+        players.set(client.sessionId, player);
+        if(!this.sessionIdQueue.includes(client.sessionId)) {
+            this.sessionIdQueue.push(client.sessionId.toString());
+        }
+        console.log(`join player, ${client.sessionId}`);
+        console.log(`userId : , ${player.zepetoUserId}`);
+
+
+        this.RequestUserId(player.zepetoUserId);
+
+        // const saveDataToDatabase = async(name : string, challenge: string) => {
+            
+        //     const url = "http://ec2-15-165-203-116.ap-northeast-2.compute.amazonaws.com/index.php";
+        //     const data = {
+        //         name,
+        //         challenge
+        //     };
+        //     const headers = {
+        //         'Content-Type' : 'application/json',
+        //         'Accept' : '*/*'
+        //         // 'ApplicationXml' : 'application/xml',
+        //         // 'ApplicationUrlEncoded' : 'application/x-www-form-urlencoded',
+        //         // 'TextPlain' : 'text/plain',
+        //         // 'TextXml' : 'text/xml'
+        //     };
+
+        //     try{
+        //         const request = await HttpService.postAsync(url, JSON.stringify(data),  headers);
+             
+        //         if(request.statusCode!== 200){
+        //             console.error('HTTP ERROR');
+        //             return
+        //         }
+    
+        //         const json = JSON.parse(request.response);
+                
+        //     }catch(error){
+        //         console.log(error);
+        //         console.log(`data : ${data}`);
+        //     }
+    
+        // };
+    
+        // saveDataToDatabase(client.sessionId,"wakeup");
+
+        // const name: string = "kangsinsung";
+        // const challenge: string = "ssssssss";
+       
+        
+    }
+
+    onLeave(client: SandboxPlayer, consented?: boolean) {
+        console.log(`leave player, ${client.sessionId}`);
+
+        this.state.players.delete(client.sessionId);
+        if(this.sessionIdQueue.includes(client.sessionId)) {
+            const leavePlayerIndex = this.sessionIdQueue.indexOf(client.sessionId);
+            this.sessionIdQueue.splice(leavePlayerIndex, 1);
+            if (leavePlayerIndex == 0) {
+                console.log(`master->, ${this.sessionIdQueue[0]}`);
+                this.broadcast(MESSAGE.MasterResponse, this.sessionIdQueue[0]);
+            }
+        }
+    }
+
+        RequestUserId(zepetoUserId : String) {
+        const url: string = "http://ec2-15-165-203-116.ap-northeast-2.compute.amazonaws.com/index.php";
+        // const url: string = "http://localhost:8080/member01/mem_models/sinsung_dao.jsp";
+
+        const headers:HttpHeader = {
+            'Content-Type': 'application/json',
+            // 'Accept': '*/*',
+            // // 'Content-Length': 0
+        };
+
+        const body:HttpBodyType = {
+            "name": zepetoUserId,
+            "challenge": "miracle morning",
+        };
+
+        HttpService.postAsync(url, body, headers).then(
+           
+            (res: HttpResponse) => {
+                
+                console.log(`HTTP Result: ${JSON.stringify(res)}`);
+            });
+    }
+}
+
+interface syncTween {
+    Id: string,
+    position: sVector3,
+    nextIndex: number,
+    loopCount: number,
+    sendTime: number,
+}
+
+interface InstantiateObj{
+    Id:string;
+    prefabName:string;
+    ownerSessionId?:string;
+    spawnPosition?:sVector3;
+    spawnRotation?:sQuaternion;
+}
+
+/** racing game **/
+interface GameReport{
+    playerUserId : string;
+    playerLapTime : number;
+}
+
+enum MESSAGE {
+    SyncPlayer = "SyncPlayer",
+    SyncTransform = "SyncTransform",
+    SyncTransformStatus = "SyncTransformStatus",
+    ChangeOwner = "ChangeOwner",
+    Instantiate = "Instantiate",
+    RequestInstantiateCache = "RequestInstantiateCache",
+    ResponsePosition = "ResponsePosition",
+    SyncDOTween = "SyncDOTween",
+    CheckServerTimeRequest = "CheckServerTimeRequest",
+    CheckServerTimeResponse = "CheckServerTimeResponse",
+    CheckMaster = "CheckMaster",
+    MasterResponse = "MasterResponse",
+    PauseUser = "PauseUser",
+    UnPauseUser = "UnPauseUser",
+
+    /** Sample Code **/
+    BlockEnter = "BlockEnter",
+    BlockExit = "BlockExit",
+    SendBlockEnterCache = "SendBlockEnterCache",
+    CoinAcquired = "CoinAcquired",
+
+    /** Racing Game **/
+    StartRunningRequest = "StartRunningRequest",
+    FinishPlayer = "FinishPlayer",
+    FirstPlayerGetIn = "FirstPlayerGetIn",
+    CountDownStart = "CountDownStart",
+    ResponseGameReport = "ResponseGameReport"
+}
+
