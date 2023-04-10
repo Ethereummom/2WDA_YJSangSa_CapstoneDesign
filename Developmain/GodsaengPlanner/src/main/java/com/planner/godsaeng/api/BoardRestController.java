52779e9ba5df5306d93be933a4f1e8a4be007e0f
Ethereummom <103083686+Ethereummom@users.noreply.github.com>
2023-04-10 10:21:40 +0900

    Mainver1.0.0

---
+++ Developmain/GodsaengPlanner/src/main/java/com/planner/godsaeng/api/BoardRestController.java
@@ -0,0 +1,9 @@
+package com.planner.godsaeng.api;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardRestController {
+}
